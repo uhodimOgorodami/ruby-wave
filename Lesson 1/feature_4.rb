ecac80da560261f5628b5327cf74dd4812dadf90
Uhodim <kroomzok@gmail.com>
2018-03-24 19:04:23 +0300

    some corrections

--- Lesson 1/feature_4.rb
+++ Lesson 1/feature_4.rb
@@ -9,14 +9,13 @@ b = gets.chomp.to_f
 puts "Задайте коэффициент c:"
 c = gets.chomp.to_f
 
-d = b ** 2 - 4 * a  * c
+d = b**2 - 4 * a * c
 
 if d > 0
-	puts "Дискриминант = #{d}"
-	puts "x1 = #{(-1 * b + Math.sqrt(d)) / (2 * a)}"
-	puts "x2 = #{(-1 * b - Math.sqrt(d)) / (2 * a)}"
-elsif
-  d == 0
+  puts "Дискриминант = #{d}"
+  puts "x1 = #{(-1 * b + Math.sqrt(d)) / (2 * a)}"
+  puts "x2 = #{(-1 * b - Math.sqrt(d)) / (2 * a)}"
+elsif  d.zero?
   puts "Дискриминант = #{d}"
   puts "x = #{(- b - d)/(2 * a)}"
 else
